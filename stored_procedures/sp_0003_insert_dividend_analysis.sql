eb13f6334bca560063992ef746c219a70835a828
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2020-02-16 08:13:17 -0300

    Adjustments

--- stored_procedures/sp_0003_insert_dividend_analysis.sql
+++ stored_procedures/sp_0003_insert_dividend_analysis.sql
@@ -1,4 +1,4 @@
-CREATE PROCEDURE SP_INSERT_DIVIDEND_ANALYSIS( 
+ALTER PROCEDURE SP_INSERT_DIVIDEND_ANALYSIS( 
 @STOCK_CODE                  VARCHAR (6) 
 ,@COMPANY                    VARCHAR (MAX) 
 ,@SECTOR                     VARCHAR (MAX) 
@@ -22,53 +22,81 @@ CREATE PROCEDURE SP_INSERT_DIVIDEND_ANALYSIS(
 ,@HAS_DIVIDEND_GRWTH_5_YRS   INT 
 ,@HAS_NET_PROFIT_REG_5_YRS   INT) 
 AS 
-    INSERT INTO DIVIDENDS 
-                (STOCK_CODE 
-                 ,COMPANY 
-                 ,SECTOR 
-                 ,SECOND_SECTOR 
-                 ,STOCK_PRICE 
-                 ,STOCK_TYPE 
-                 ,VALUATION 
-                 ,STOCK_AVAILABLE_VOLUME 
-                 ,VOL_NEGOTIATED_LAST_21 
-                 ,DIVIDEND_LAST_PRICE 
-                 ,COMPANY_NET_PROFIT 
-                 ,DIVIDEND_YELD 
-                 ,DIVIDEND_AVG_PAYOUT_12_MOS 
-                 ,DIVIDEND_AVG_PAYOUT_5_YRS 
-                 ,MAJOR_SHARE_HOLDER 
-                 ,COMPANY_ROE 
-                 ,COMPANY_ROE_5_YRS 
-                 ,COMP_GROSSDEBT_EBTIDA 
-                 ,DIVIDEND_YIELD_5_YRS 
-                 ,HAS_DIVIDEND_SRD_5_YRS 
-                 ,HAS_DIVIDEND_GRWTH_5_YRS 
-                 ,HAS_NET_PROFIT_REG_5_YRS 
-                 ,DT_LAST_UPDATE) 
-    VALUES      ( @STOCK_CODE 
-                  ,@COMPANY 
-                  ,@SECTOR 
-                  ,@SECOND_SECTOR 
-                  ,@STOCK_PRICE 
-                  ,@STOCK_TYPE 
-                  ,@VALUATION 
-                  ,@STOCK_AVAILABLE_VOLUME 
-                  ,@VOL_NEGOTIATED_LAST_21 
-                  ,@DIVIDEND_LAST_PRICE 
-                  ,@COMPANY_NET_PROFIT 
-                  ,@DIVIDEND_YELD 
-                  ,@DIVIDEND_AVG_PAYOUT_12_MOS 
-                  ,@DIVIDEND_AVG_PAYOUT_5_YRS 
-                  ,@MAJOR_SHARE_HOLDER 
-                  ,@COMPANY_ROE 
-                  ,@COMPANY_ROE_5_YRS 
-                  ,@COMP_GROSSDEBT_EBTIDA 
-                  ,@DIVIDEND_YIELD_5_YRS 
-                  ,@HAS_DIVIDEND_SRD_5_YRS 
-                  ,@HAS_DIVIDEND_GRWTH_5_YRS 
-                  ,@HAS_NET_PROFIT_REG_5_YRS
-                  ,GETDATE()
-                ); 
-
+    IF EXISTS (SELECT STOCK_CODE FROM DIVIDENDS WHERE STOCK_CODE = @STOCK_CODE AND DT_LAST_UPDATE = GETDATE())
+      UPDATE DIVIDENDS SET 
+          STOCK_CODE = @STOCK_CODE 
+          ,COMPANY = @COMPANY 
+          ,SECTOR = @SECTOR 
+          ,SECOND_SECTOR = @SECOND_SECTOR 
+          ,STOCK_PRICE = @STOCK_PRICE 
+          ,STOCK_TYPE = @STOCK_TYPE 
+          ,VALUATION = @VALUATION 
+          ,STOCK_AVAILABLE_VOLUME = @STOCK_AVAILABLE_VOLUME 
+          ,VOL_NEGOTIATED_LAST_21 = @VOL_NEGOTIATED_LAST_21 
+          ,DIVIDEND_LAST_PRICE = @DIVIDEND_LAST_PRICE 
+          ,COMPANY_NET_PROFIT = @COMPANY_NET_PROFIT 
+          ,DIVIDEND_YELD = @DIVIDEND_YELD 
+          ,DIVIDEND_AVG_PAYOUT_12_MOS = @DIVIDEND_AVG_PAYOUT_12_MOS 
+          ,DIVIDEND_AVG_PAYOUT_5_YRS = @DIVIDEND_AVG_PAYOUT_5_YRS 
+          ,MAJOR_SHARE_HOLDER = @MAJOR_SHARE_HOLDER 
+          ,COMPANY_ROE = @COMPANY_ROE 
+          ,COMPANY_ROE_5_YRS = @COMPANY_ROE_5_YRS 
+          ,COMP_GROSSDEBT_EBTIDA = @COMP_GROSSDEBT_EBTIDA 
+          ,DIVIDEND_YIELD_5_YRS = @DIVIDEND_YIELD_5_YRS 
+          ,HAS_DIVIDEND_SRD_5_YRS = @HAS_DIVIDEND_SRD_5_YRS 
+          ,HAS_DIVIDEND_GRWTH_5_YRS = @HAS_DIVIDEND_GRWTH_5_YRS 
+          ,HAS_NET_PROFIT_REG_5_YRS = @HAS_NET_PROFIT_REG_5_YRS 
+          ,DT_LAST_UPDATE = GETDATE()
+      WHERE 
+          STOCK_CODE = @STOCK_CODE 
+          AND DT_LAST_UPDATE = GETDATE()
+    ELSE
+      INSERT INTO DIVIDENDS 
+                  (STOCK_CODE 
+                  ,COMPANY 
+                  ,SECTOR 
+                  ,SECOND_SECTOR 
+                  ,STOCK_PRICE 
+                  ,STOCK_TYPE 
+                  ,VALUATION 
+                  ,STOCK_AVAILABLE_VOLUME 
+                  ,VOL_NEGOTIATED_LAST_21 
+                  ,DIVIDEND_LAST_PRICE 
+                  ,COMPANY_NET_PROFIT 
+                  ,DIVIDEND_YELD 
+                  ,DIVIDEND_AVG_PAYOUT_12_MOS 
+                  ,DIVIDEND_AVG_PAYOUT_5_YRS 
+                  ,MAJOR_SHARE_HOLDER 
+                  ,COMPANY_ROE 
+                  ,COMPANY_ROE_5_YRS 
+                  ,COMP_GROSSDEBT_EBTIDA 
+                  ,DIVIDEND_YIELD_5_YRS 
+                  ,HAS_DIVIDEND_SRD_5_YRS 
+                  ,HAS_DIVIDEND_GRWTH_5_YRS 
+                  ,HAS_NET_PROFIT_REG_5_YRS 
+                  ,DT_LAST_UPDATE) 
+      VALUES      ( @STOCK_CODE 
+                    ,@COMPANY 
+                    ,@SECTOR 
+                    ,@SECOND_SECTOR 
+                    ,@STOCK_PRICE 
+                    ,@STOCK_TYPE 
+                    ,@VALUATION 
+                    ,@STOCK_AVAILABLE_VOLUME 
+                    ,@VOL_NEGOTIATED_LAST_21 
+                    ,@DIVIDEND_LAST_PRICE 
+                    ,@COMPANY_NET_PROFIT 
+                    ,@DIVIDEND_YELD 
+                    ,@DIVIDEND_AVG_PAYOUT_12_MOS 
+                    ,@DIVIDEND_AVG_PAYOUT_5_YRS 
+                    ,@MAJOR_SHARE_HOLDER 
+                    ,@COMPANY_ROE 
+                    ,@COMPANY_ROE_5_YRS 
+                    ,@COMP_GROSSDEBT_EBTIDA 
+                    ,@DIVIDEND_YIELD_5_YRS 
+                    ,@HAS_DIVIDEND_SRD_5_YRS 
+                    ,@HAS_DIVIDEND_GRWTH_5_YRS 
+                    ,@HAS_NET_PROFIT_REG_5_YRS
+                    ,GETDATE()
+                  ); 
 GO 
