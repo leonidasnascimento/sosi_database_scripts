bfbc19a18b6e181d26435c97d664c8682f1773cc
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2020-02-16 08:02:26 -0300

    Adjustmens on SP

--- stored_procedures/sp_0001_get_dividend_analysis.sql
+++ stored_procedures/sp_0001_get_dividend_analysis.sql
@@ -5,35 +5,143 @@ SET quoted_identifier ON
 GO 
 
 ALTER PROCEDURE [dbo].[Sp_get_dividend_analysis](@stock_code VARCHAR(6)) 
-AS 
-    SELECT stock_code, 
-           company, 
-           sector, 
-           second_sector, 
-           stock_price, 
-           stock_type, 
-           valuation, 
-           stock_available_volume, 
-           vol_negotiated_last_21, 
-           dividend_last_price, 
-           company_net_profit, 
-           dividend_yeld, 
-           dividend_avg_payout_12_mos, 
-           dividend_avg_payout_5_yrs, 
-           major_share_holder, 
-           company_roe, 
-           company_roe_5_yrs, 
-           comp_grossdebt_ebtida, 
-           dividend_yield_5_yrs, 
-           has_dividend_srd_5_yrs, 
-           has_dividend_grwth_5_yrs, 
-           has_net_profit_reg_5_yrs, 
-           dt_last_update 
-    FROM   dbo.dividends 
-    WHERE  dt_last_update = (SELECT TOP 1 dt_last_update 
-                             FROM   dbo.dividends 
-                             ORDER  BY dt_last_update DESC) 
-           AND stock_code = IIF(@value = '', stock_code, 
-                            ISNULL(@value, stock_code)) 
-    ORDER  BY stock_code; 
-GO
+AS
+       DROP TABLE IF EXISTS #UPDT_ITEMS 
+       DROP TABLE IF EXISTS #NOT_UPDT_ITEMS 
+
+       SELECT * 
+       INTO   #UPDT_ITEMS 
+       FROM   ( 
+                     SELECT STOCK_CODE, 
+                            COMPANY, 
+                            SECTOR, 
+                            SECOND_SECTOR, 
+                            STOCK_PRICE, 
+                            STOCK_TYPE, 
+                            VALUATION, 
+                            STOCK_AVAILABLE_VOLUME, 
+                            VOL_NEGOTIATED_LAST_21, 
+                            DIVIDEND_LAST_PRICE, 
+                            COMPANY_NET_PROFIT, 
+                            DIVIDEND_YELD, 
+                            DIVIDEND_AVG_PAYOUT_12_MOS, 
+                            DIVIDEND_AVG_PAYOUT_5_YRS, 
+                            MAJOR_SHARE_HOLDER, 
+                            COMPANY_ROE, 
+                            COMPANY_ROE_5_YRS, 
+                            COMP_GROSSDEBT_EBTIDA, 
+                            DIVIDEND_YIELD_5_YRS, 
+                            HAS_DIVIDEND_SRD_5_YRS, 
+                            HAS_DIVIDEND_GRWTH_5_YRS, 
+                            HAS_NET_PROFIT_REG_5_YRS, 
+                            DT_LAST_UPDATE 
+                     FROM   DIVIDENDS 
+                     WHERE  DT_LAST_UPDATE = 
+                            ( 
+                                   SELECT TOP 1 
+                                          DT_LAST_UPDATE 
+                                   FROM     DIVIDENDS 
+                                   ORDER BY DT_LAST_UPDATE DESC) 
+                            AND stock_code = IIF(@stock_code = '', stock_code, ISNULL(@stock_code, stock_code)) 
+              ) AS UPDATED_ITEMS
+
+       SELECT * 
+       INTO   #NOT_UPDT_ITEMS 
+       FROM   ( 
+                     SELECT   STOCK_CODE, 
+                            COMPANY, 
+                            SECTOR, 
+                            SECOND_SECTOR, 
+                            STOCK_PRICE, 
+                            STOCK_TYPE, 
+                            VALUATION, 
+                            STOCK_AVAILABLE_VOLUME, 
+                            VOL_NEGOTIATED_LAST_21, 
+                            DIVIDEND_LAST_PRICE, 
+                            COMPANY_NET_PROFIT, 
+                            DIVIDEND_YELD, 
+                            DIVIDEND_AVG_PAYOUT_12_MOS, 
+                            DIVIDEND_AVG_PAYOUT_5_YRS, 
+                            MAJOR_SHARE_HOLDER, 
+                            COMPANY_ROE, 
+                            COMPANY_ROE_5_YRS, 
+                            COMP_GROSSDEBT_EBTIDA, 
+                            DIVIDEND_YIELD_5_YRS, 
+                            HAS_DIVIDEND_SRD_5_YRS, 
+                            HAS_DIVIDEND_GRWTH_5_YRS, 
+                            HAS_NET_PROFIT_REG_5_YRS, 
+                            Max(DT_LAST_UPDATE) AS dt_last_update 
+                     FROM     DIVIDENDS D 
+                     WHERE    D.STOCK_CODE NOT IN (SELECT STOCK_CODE FROM   #UPDT_ITEMS) 
+                              AND D.STOCK_CODE = IIF(@stock_code = '', stock_code, ISNULL(@stock_code, stock_code))     
+                     GROUP BY STOCK_CODE, 
+                            COMPANY, 
+                            SECTOR, 
+                            SECOND_SECTOR, 
+                            STOCK_PRICE, 
+                            STOCK_TYPE, 
+                            VALUATION, 
+                            STOCK_AVAILABLE_VOLUME, 
+                            VOL_NEGOTIATED_LAST_21, 
+                            DIVIDEND_LAST_PRICE, 
+                            COMPANY_NET_PROFIT, 
+                            DIVIDEND_YELD, 
+                            DIVIDEND_AVG_PAYOUT_12_MOS, 
+                            DIVIDEND_AVG_PAYOUT_5_YRS, 
+                            MAJOR_SHARE_HOLDER, 
+                            COMPANY_ROE, 
+                            COMPANY_ROE_5_YRS, 
+                            COMP_GROSSDEBT_EBTIDA, 
+                            DIVIDEND_YIELD_5_YRS, 
+                            HAS_DIVIDEND_SRD_5_YRS, 
+                            HAS_DIVIDEND_GRWTH_5_YRS, 
+                            HAS_NET_PROFIT_REG_5_YRS ) AS NOT_UPDATED_ITEMS
+       SELECT STOCK_CODE, 
+              COMPANY, 
+              SECTOR, 
+              SECOND_SECTOR, 
+              STOCK_PRICE, 
+              STOCK_TYPE, 
+              VALUATION, 
+              STOCK_AVAILABLE_VOLUME, 
+              VOL_NEGOTIATED_LAST_21, 
+              DIVIDEND_LAST_PRICE, 
+              COMPANY_NET_PROFIT, 
+              DIVIDEND_YELD, 
+              DIVIDEND_AVG_PAYOUT_12_MOS, 
+              DIVIDEND_AVG_PAYOUT_5_YRS, 
+              MAJOR_SHARE_HOLDER, 
+              COMPANY_ROE, 
+              COMPANY_ROE_5_YRS, 
+              COMP_GROSSDEBT_EBTIDA, 
+              DIVIDEND_YIELD_5_YRS, 
+              HAS_DIVIDEND_SRD_5_YRS, 
+              HAS_DIVIDEND_GRWTH_5_YRS, 
+              HAS_NET_PROFIT_REG_5_YRS, 
+              DT_LAST_UPDATE 
+       FROM   #UPDT_ITEMS 
+       UNION ALL 
+       SELECT STOCK_CODE, 
+              COMPANY, 
+              SECTOR, 
+              SECOND_SECTOR, 
+              STOCK_PRICE, 
+              STOCK_TYPE, 
+              VALUATION, 
+              STOCK_AVAILABLE_VOLUME, 
+              VOL_NEGOTIATED_LAST_21, 
+              DIVIDEND_LAST_PRICE, 
+              COMPANY_NET_PROFIT, 
+              DIVIDEND_YELD, 
+              DIVIDEND_AVG_PAYOUT_12_MOS, 
+              DIVIDEND_AVG_PAYOUT_5_YRS, 
+              MAJOR_SHARE_HOLDER, 
+              COMPANY_ROE, 
+              COMPANY_ROE_5_YRS, 
+              COMP_GROSSDEBT_EBTIDA, 
+              DIVIDEND_YIELD_5_YRS, 
+              HAS_DIVIDEND_SRD_5_YRS, 
+              HAS_DIVIDEND_GRWTH_5_YRS, 
+              HAS_NET_PROFIT_REG_5_YRS, 
+              DT_LAST_UPDATE 
+       FROM   #NOT_UPDT_ITEMS
